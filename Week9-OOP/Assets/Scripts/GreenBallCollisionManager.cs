4b54b6a0f052503e30fb619980e292df380d6e2f
Paul Pomeroy <ppom@udel.edu>
2022-11-28 23:49:33 -0500

    Added unique behaviors for collision to the red and green balls.

---
+++ Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public AudioSource vineBoom;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        // Plays the infamous "Vine Boom" sound effect when the green ball collides with another ball.
+        vineBoom.Play();
+    }
+}
+
---
+++ Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        // The red ball gets bigger when it collides with another object.
+        transform.localScale += new Vector3(1, 1, 1);
+    }
+}
+
